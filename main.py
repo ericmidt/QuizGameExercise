7fda72a00e309b775bf536e33f32eb9004f9a4d7
Eric <ericschmidt49@gmail.com>
2023-05-16 19:25:41 -0300

    Add GUI and API request.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -10,6 +11,8 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
 still_has_questions = True
 
 while still_has_questions:
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,24 +1,27 @@
+import html
+
+
 class QuizBrain:
 
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
         self.score = 0
+        self.answer = self.question_list[self.question_number]
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
+        self.answer = self.question_list[self.question_number]
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer)
+        return f"Q.{self.question_number}: {html.unescape(current_question.text)} (True/False): "
 
-    def check_answer(self, user_answer, correct_answer):
-        if user_answer.lower() == correct_answer.lower():
-            print("You got it right!")
+    def check_answer(self, user_answer):
+        correct_answer = self.answer.answer
+        if user_answer == correct_answer:
             self.score += 1
+            return True
         else:
-            print("That's wrong.")
-        print(f"The correct answer was: {correct_answer}")
-        print(f"Your current score is: {self.score}/{self.question_number}\n")
+            return False
